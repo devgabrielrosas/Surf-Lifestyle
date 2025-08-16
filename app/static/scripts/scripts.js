cf73f1fa40518c7c511e9b3a294964040bcade5c
Gabriel Rosas <rosas.gabriel2007@gmail.com>
2025-08-16 00:14:08 -0300

    feat: Finalizei página inicial

--- app/static/scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-  const header = document.getElementById('header-secundario');
-  if (window.scrollY > 70) { 
-    header.classList.add('show');
-  } else {
-    header.classList.remove('show');
-  }
-});
