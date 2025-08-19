8cb53a75988344dba30fa80c2b70012a3db4b9b6
Thalyson <thalyson.felicio@academico.ifpb.edu.br>
2025-08-19 19:40:30 -0300

    add: Adicionei o menu e nosso blog

--- app/static/scripts/menu.js
+++ app/static/scripts/menu.js
@@ -10,3 +10,12 @@ function selectLink(){
 menuItem.forEach((item)=>
     item.addEventListener('click', selectLink)
 )
+
+// expndir o menu
+
+var btnExp = document.querySelector('#btn-exp')
+var menuSite = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSite.classList.toggle('expandir')
+})
