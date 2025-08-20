208b015151eef766f49137f35f04fb16048b690a
Jairo <jairoccostajr@gmail.com>
2025-08-20 00:18:15 -0300

    Adiciona persistência de dados do formulário num arquivo .csv

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from .main import main as main_bp
+from . import storage
 
 def create_app():
     app = Flask(__name__)
-    # app.secret_key = 'secret'
+
+    storage.inicializar_csv()
+    
+    app.posts = storage.ler_csv()
+
+    app.secret_key = 'projeto-surf'
+
     app.register_blueprint(main_bp)
     
     return app
--- app/main/forms.py
+++ app/main/forms.py
@@ -1,15 +1,43 @@
 from flask import Blueprint, request, redirect, url_for, render_template, current_app # type: ignore
 import requests # type: ignore
+from app import storage
 from . import main
 
-MAKE_WEBOHOOK_URL = "https://hook.us2.make.com/xfk1lnl2kie69u8taswfccd6176k11rx"
+MAKE_WEBHOOK_URL = "https://hook.us2.make.com/xfk1lnl2kie69u8taswfccd6176k11rx"
+
 
 @main.route("/", methods=["GET", "POST"])
 def enviar():
+    mensagem = None
+    
     if request.method == 'POST':
-        nome = request.form.get("nome")
-        email = request.form.get("email")
-        data = {'email': email, 'nome': nome}
-        r = requests.post(MAKE_WEBOHOOK_URL, json=data)
+        nome = request.form['nome']
+        email = request.form['email']
+        
+        data = {'nome': nome, 'email': email}
+        requests.post(MAKE_WEBHOOK_URL, json=data)
+        
+        current_app.posts.append({'nome': nome, 'email': email})
+        storage.escrever_csv(nome, email)
+
         mensagem = 'Cadastro concluído!'
-        return render_template('index.html', mensagem=mensagem)
+        return redirect(url_for('main.enviar'))
+    
+    return render_template('index.html', mensagem=mensagem)
+
+
+@main.route('/enviar_inscritos')
+def enviar_inscritos_csv():
+    token = request.args.get("token")
+    if token != "alan_jairo_rosas_thalyson":
+        return "Acesso não autorizado", 403
+
+    inscritos = storage.ler_csv()
+    for inscrito in inscritos:
+        data = {
+            'nome': inscrito['nome'],
+            'email': inscrito['email']
+        }
+        requests.post(..., json=data)
+        print(inscrito)
+    return "Inscrições enviadas para o Make!"
